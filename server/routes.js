2084ae3626c5ab819742dc165d68216a1d91d40b
Alex Colombo <uricaine@hotmail.com>
2019-03-25 19:25:54 +0100

    adding mesaje

--- server/routes.js
+++ server/routes.js
@@ -2,18 +2,28 @@
 var express = require('express');
 var router = express.Router();
 
-
-
-
-
-
+const mensajes = [
+	{
+		_id: 'XXX',
+		user: 'spiderman',
+		mensaje: 'Hola mundo'
+	},
+	{
+		_id: 'XXX',
+		user: 'ironman',
+		mensaje: 'Hola mundo'
+	},
+	{
+		_id: 'XXX',
+		user: 'hulk',
+		mensaje: 'Hola mundo'
+	}
+];
 
 // Get mensajes
-router.get('/', function (req, res) {
-  res.json('Obteniendo mensajes');
+router.get('/', function(req, res) {
+	// res.json('Obteniendo mensajes' + JSON.stringify(mensajes));
+	res.json(mensajes);
 });
-
-
-
 
 module.exports = router;
