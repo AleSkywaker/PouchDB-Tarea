d6f32970e6ad931ba017e7d9bb862d3983bbf801
Alex Colombo <uricaine@hotmail.com>
2019-03-26 19:36:35 +0100

    upgrade sw

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v1';
+const STATIC_CACHE = 'static-v2';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
@@ -56,6 +56,8 @@ self.addEventListener('fetch', (e) => {
 
 	if (e.request.url.includes('/api')) {
 		//return respuesta
+       respuesta = manejoApiMensajes(DYNAMIC_CACHE, e.request)
+
 	} else {
 		respuesta = caches.match(e.request).then((res) => {
 			if (res) {
