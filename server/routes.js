d593d5a8c35d62d782c5ddae631c2c017a3784bf
Alex Colombo <uricaine@hotmail.com>
2019-03-27 20:19:47 +0100

    new method in route.js

--- server/routes.js
+++ server/routes.js
@@ -37,4 +37,22 @@ router.get('/', function(req, res) {
 	res.json(mensajes);
 });
 
+
+//Post mensaje
+router.post('/', function(req, res){
+
+    const mensaje = {
+		mensaje: req.body.mensaje,
+		user: req.body.user
+	}
+
+	mensajes.push(mensaje)
+
+   res.json({
+	   ok:true,
+	   mensaje,
+	   mensajes
+   })
+
+})
 module.exports = router;
