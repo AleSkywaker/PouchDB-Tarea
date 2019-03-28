b2fff35a3e95079fa91f4d3edcd08e21e8dcb4a7
Alex Colombo <uricaine@hotmail.com>
2019-03-28 19:51:15 +0100

    store data in indexDB

--- public/js/sw-utils.js
+++ public/js/sw-utils.js
@@ -25,18 +25,24 @@ function actualizaCacheStatico(staticCache, req, APP_SHELL_INMUTABLE) {
 }
 
 //Network with cache fallback / update
-
 function manejoApiMensajes(cacheName, req) {
-	return fetch( req ).then( res => {
-    
-		if ( res.ok ) {
-			actualizaCacheDinamico( cacheName, req, res.clone() );
-			return res.clone();
-		} else {
-			return caches.match( req );
-		}
-  
-	}).catch( err => {
-		return caches.match( req );
-	});
+	if (req.clone().method === 'POST') {
+		//Posteo de un nuevo mensaje
+
+		// Guardar en indexDB
+		return fetch (req)
+	} else {
+		return fetch(req)
+			.then((res) => {
+				if (res.ok) {
+					actualizaCacheDinamico(cacheName, req, res.clone());
+					return res.clone();
+				} else {
+					return caches.match(req);
+				}
+			})
+			.catch((err) => {
+				return caches.match(req);
+			});
+	}
 }
