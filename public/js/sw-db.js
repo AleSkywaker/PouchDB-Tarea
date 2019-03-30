f4f41af4978be5b8ab2a71b035d928476e0680e6
Alex Colombo <uricaine@hotmail.com>
2019-03-30 11:28:29 +0100

    register task in sw

--- public/js/sw-db.js
+++ public/js/sw-db.js
@@ -5,6 +5,10 @@ function guardarMensaje(mensaje) {
 	mensaje._id = new Date().toISOString();
 
 	db.put(mensaje).then(() => {
+  
+		self.registration.sync.register('nuevo-post')
+		
+
 		console.log('Mensaje guardado para posterior posteo');
 	});
 }
