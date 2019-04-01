d1a11e2466fe7cf945f47da6642742c5cb5f880f
Alex Colombo <uricaine@hotmail.com>
2019-04-01 18:43:24 +0200

    deleting static properties

--- server/routes.js
+++ server/routes.js
@@ -2,43 +2,44 @@
 var express = require('express');
 var router = express.Router();
 
+
 const mensajes = [
-	{
-		_id: 'XXX',
-		user: 'spiderman',
-		mensaje: 'Hola mundo'
-	},
-	{
-		_id: 'XXX',
-		user: 'ironman',
-		mensaje: 'Hola mundo'
-	},
-	{
-		_id: 'XXX',
-		user: 'hulk',
-		mensaje: 'Hola mundo'
-	}
+
+  {
+    _id: 'XXX',
+    user: 'spiderman',
+    mensaje: 'Hola Mundo'
+  }
+
 ];
 
+
 // Get mensajes
-router.get('/', function(req, res) {
-	// res.json('Obteniendo mensajes' + JSON.stringify(mensajes));
-	res.json(mensajes);
+router.get('/', function (req, res) {
+  // res.json('Obteniendo mensajes');
+  res.json( mensajes );
 });
 
-//Post mensaje
-router.post('/', function(req, res) {
-	const mensaje = {
-		mensaje: req.body.mensaje,
-		user: req.body.user
-	};
 
-	mensajes.push(mensaje);
+// Post mensaje
+router.post('/', function (req, res) {
+  
+  const mensaje = {
+    mensaje: req.body.mensaje,
+    user: req.body.user
+  };
 
-	console.log(mensaje);
-	res.json({
-		ok: true,
-		mensaje
-	});
+  mensajes.push( mensaje );
+
+  console.log(mensajes);
+
+
+  res.json({
+    ok: true,
+    mensaje
+  });
 });
+
+
+
 module.exports = router;
