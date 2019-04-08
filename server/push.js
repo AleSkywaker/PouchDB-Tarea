7f0b5112c56436aabf643a14b1752346c9aa188c
Alex Colombo <uricaine@hotmail.com>
2019-04-08 20:52:14 +0200

    creating vapid key

--- server/push.js
+++ server/push.js
@@ -1,7 +1,6 @@
-const urlsafeBase64 = require('urlsafe-base64')
-const vapid = require('./vapid.json')
-
+const urlsafeBase64 = require('urlsafe-base64');
+const vapid = require('./vapid.json');
 
 module.exports.getKey = () => {
-    return vapid.publicKey;
-}
+	return urlsafeBase64.decode(vapid.publicKey);
+};
