046ba30e92c98b8b78e281107563c3dcf13983d7
Alex Colombo <uricaine@hotmail.com>
2019-04-10 23:13:42 +0200

    reg susbscription

--- public/js/app.js
+++ public/js/app.js
@@ -11,6 +11,7 @@ if (navigator.serviceWorker) {
 	window.addEventListener('load', function() {
 		navigator.serviceWorker.register(swLocation).then(function(reg){
 			swReg = reg;
+			swReg.pushManager.getSubscription();
 		})
 	});
 }
