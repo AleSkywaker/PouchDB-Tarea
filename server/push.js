20f429a7d3ce60ce347a9e534d353c713b64e7a2
Alex Colombo <uricaine@hotmail.com>
2019-04-14 23:44:06 +0200

    push suscription

--- server/push.js
+++ server/push.js
@@ -1,6 +1,13 @@
 const urlsafeBase64 = require('urlsafe-base64');
 const vapid = require('./vapid.json');
 
+const suscripciones = [];
+
 module.exports.getKey = () => {
 	return urlsafeBase64.decode(vapid.publicKey);
 };
+
+module.exports.addSubscription = (sus) =>{
+	suscripciones.push(sus);
+	console.log("Back end", suscripciones)
+}
