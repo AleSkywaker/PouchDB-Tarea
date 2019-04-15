41ac7a553dd8b6d787fcc0134916bfe457a36274
Alex Colombo <uricaine@hotmail.com>
2019-04-15 11:00:23 +0200

    add file system node

--- server/push.js
+++ server/push.js
@@ -11,6 +11,6 @@ module.exports.getKey = () => {
 module.exports.addSubscription = (sus) => {
 	suscripciones.push(sus);
 
-	fs.writeFileSync('${__dirname}/subs-db.json')
+	fs.writeFileSync('${__dirname}/subs-db.json', JSON.stringify(suscripciones))
 	console.log('Back end', suscripciones);
 };
