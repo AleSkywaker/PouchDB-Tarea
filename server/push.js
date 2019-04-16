08d657d390b6a4fbed43f5b07ca0c253c2469651
Alex Colombo <uricaine@hotmail.com>
2019-04-16 22:47:52 +0200

    undo vapid key

--- server/push.js
+++ server/push.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const urlsafeBase64 = require('urlsafe-base64');
-// const vapid = require('./vapid.json');
+const vapid = require('./vapid.json');
 
 const suscripciones = [];
 
-// module.exports.getKey = () => {
-// 	return urlsafeBase64.decode(vapid.publicKey);
-// };
+module.exports.getKey = () => {
+	return urlsafeBase64.decode(vapid.publicKey);
+};
 
 module.exports.addSubscription = (sus) => {
 	suscripciones.push(sus);
