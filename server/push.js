1b125837ee559098990dcb6f9a289481220e9fe2
Alex Colombo <uricaine@hotmail.com>
2019-04-27 10:33:58 +0200

    cleaning old notifications

--- server/push.js
+++ server/push.js
@@ -5,36 +5,42 @@ const vapid = require('./vapid.json');
 const webpush = require('web-push');
 
 webpush.setVapidDetails(
-	'mailto:uricaine@hotmail.com',
-	vapid.publicKey,
-	vapid.privateKey
-  );
+    'mailto:uricaine@hotmail.com',
+    vapid.publicKey,
+    vapid.privateKey
+);
 
 //Make sure the json file is not empty.
-const suscripciones =  require('./subs-db.json');
+let suscripciones = require('./subs-db.json');
 
 module.exports.getKey = () => {
-	return urlsafeBase64.decode(vapid.publicKey);
+    return urlsafeBase64.decode(vapid.publicKey);
 };
 
 module.exports.addSubscription = (sus) => {
-	suscripciones.push(sus);
-	console.log('Back end', suscripciones);
-	fs.writeFileSync(`${__dirname}/subs-db.json`, JSON.stringify(suscripciones))
+    suscripciones.push(sus);
+    console.log('Back end', suscripciones);
+    fs.writeFileSync(`${__dirname}/subs-db.json`, JSON.stringify(suscripciones))
 };
 
-module.exports.sendPush = (post)=>{
-		console.log('Mandando PUSHES')
-		const notiticacionesEnviadas = [];
-    suscripciones.forEach((subs, i )=> {
-		const pushProm = webpush.sendNotification(subs, JSON.stringify(post))
-			.then(console.log('Notification enviada'))
-			.catch(err => {
-					console.log('notiticacion falló')
-					if(err.statusCode === 410 ){// GONE, ya no existe
-						suscripciones[i].borrar = true;
-					} 
-			})
-			notiticacionesEnviadas.push(pushProm)
-	});
+module.exports.sendPush = (post) => {
+    console.log('Mandando PUSHES')
+    const notiticacionesEnviadas = [];
+    suscripciones.forEach((subs, i) => {
+        const pushProm = webpush.sendNotification(subs, JSON.stringify(post))
+            .then(console.log('Notification enviada'))
+            .catch(err => {
+                console.log('notiticacion falló')
+                if (err.statusCode === 410) { // GONE, ya no existe
+                    suscripciones[i].borrar = true;
+                }
+            })
+        notiticacionesEnviadas.push(pushProm)
+    });
+
+    Promise.all(notiticacionesEnviadas).then(() => {
+        suscripciones = suscripciones.filter(subs => !subs.borrar)
+        fs.writeFileSync(`${__dirname}/subs-db.json`, JSON.stringify(suscripciones))
+    })
+
 }
