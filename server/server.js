3a2611940ea288b1230b42e8f190969899813b37
Alex Colombo <uricaine@hotmail.com>
2019-05-01 19:22:24 +0200

    test

--- server/server.js
+++ server/server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-
 const app = express();
-
 
 const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 3000;
