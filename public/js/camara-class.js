6f40e823c1d1a9c637b8f4650be55b647b379111
Alex Colombo <uricaine@hotmail.com>
2019-05-05 09:26:13 +0200

    init take a shoot

--- public/js/camara-class.js
+++ public/js/camara-class.js
@@ -17,5 +17,35 @@ class Camara {
     }
     apagar() {
 
+        this.videoNode.pause();
+
+
+        if (this.stream) {
+            this.stream.getTracks()[0].stop();
+        }
+
+    }
+    tomarFoto() {
+        //Crear elemento canvas para renderizar la foto
+        let canvas = document.createElement('canvas');
+
+        // Configurar medidas del canvas
+        canvas.setAttribute('width', 300)
+        canvas.setAttribute('height', 300)
+
+        //Obtener contexto del canvas para una simple imagen
+        let context = canvas.getContext('2d');
+
+        // Dibujar imagen dentro del canvas
+        context.drawImage(this.videoNode, 0, 0, canvas.width, canvas.height);
+
+        this.foto = context.canvas.toDataURL();
+
+        //Limpieza
+        canvas = null;
+        context = null;
+
+        return this.foto;
+
     }
 }
